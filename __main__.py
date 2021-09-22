84be2f759afc56400fbe394c09ac5dc2a68d991d
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-09-21 23:14:46 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -15,7 +15,7 @@ from cerberus import Validator
 
 
 def dataset(manifest: str):
-    bigquery.Dataset(
+    dts = bigquery.Dataset(
         resource_name=manifest['resource_name'],
         dataset_id=manifest['dataset_id'],
         description=manifest['description'],
@@ -29,11 +29,13 @@ def dataset(manifest: str):
     )
 
 
+
 def dataset_user_access(
     manifest: str,
-    user: str,
     role: str):
 
+    # readers = ["users:" + reader for reader in manifest['users']['readers']]
+    # writers = ["users:" + writer for writer in manifest['users']['writers']]
     bigquery.DatasetAccess(
         resource_name=manifest['resource_name'],
         dataset_id=manifest['dataset_id'],
