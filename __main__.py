4eedbe8d10f1623e475cd0392a05aa2cf9a94ab7
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-09-29 12:10:14 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -406,19 +406,19 @@ teams_root = '/workspace/teams/'
 manifests_set = list_manifests(teams_root)
 print('########### MANIFEST_SET')
 print(manifests_set)
-# for dep_manifest in manifests_set:
-#     for root_manifest in manifests_set:
-#         print(dep_manifest)
-#         print(root_manifest)
-#         print(get_value(dep_manifest, 'dependencies'))
-#         print(get_value(root_manifest, 'resource_name'))
+for dep_manifest in manifests_set:
+    for root_manifest in manifests_set:
+        print(dep_manifest)
+        print(root_manifest)
+        print('DEP: ' + get_value(dep_manifest, 'dependencies'))
+        print('RESOURCE: ' + get_value(root_manifest, 'resource_name'))
 
-dependent_map = [(dep_manifest, root_manifest)
-    for dep_manifest in manifests_set
-    for root_manifest in manifests_set
-    if get_value(dep_manifest, 'dependencies') and get_value(root_manifest, 'resource_name') == get_value(dep_manifest, 'dependencies')
-]
-print(dependent_map)
+# dependent_map = [(dep_manifest, root_manifest)
+#     for dep_manifest in manifests_set
+#     for root_manifest in manifests_set
+#     if get_value(dep_manifest, 'dependencies') and get_value(root_manifest, 'resource_name') == get_value(dep_manifest, 'dependencies')
+# ]
+# print(dependent_map)
 
     # if get_manifast_kind(manifest, 'table'):
     #     print('########### MANIFEST:')
