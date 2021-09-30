6c0e733fd3480342349102fbce2371370e78edbc
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-09-30 11:35:38 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -428,9 +428,9 @@ dependent_map = list(set( [(root_manifest, dep_manifest)
 
 sorted_yml = graph_sort(dependent_map).sorted
 delta_yml = list(set(manifests_set) - set(graph_sort(dependent_map).sorted))
-print(type(delta_yml))
-print(type(sorted_yml))
-print(sorted_yml.extend(delta_yml))
+sorted_yml.extend(delta_yml)
+
+print(sorted_yml)
 
 
     # if get_manifast_kind(manifest, 'table'):
