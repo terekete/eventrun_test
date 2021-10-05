ca403052ed7ac294c6b743577c6354a6301df762
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-05 15:00:56 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -403,7 +403,7 @@ teams_set = set([
 
 teams_diff = read_diff()
 for team in teams_diff:
-    # create_trigger(team)
+    create_trigger(team)
     stack = auto.create_or_select_stack(
         stack_name=team,
         project_name='eventrun',
