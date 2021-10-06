ccc734ecab2665072cb67a6d2f1b2ae7fc08c281
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-06 16:21:41 -0400

    fix: dv

--- __main__.py
+++ __main__.py
@@ -220,7 +220,7 @@ def bucket(manifest: str):
     writers = [writer for writer in manifest['users']['writers'] or []]
 
     bucket = storage.Bucket(
-        resource_name=manifest['bucket_name'],
+        manifest['bucket_name'],
         name=manifest['bucket_name'],
         force_destroy=True,
         lifecycle_rules=[storage.BucketLifecycleRuleArgs(
