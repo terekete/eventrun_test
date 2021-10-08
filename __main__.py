fd9e1dd765a0577f3b11b8da60aa3740682bba27
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-08 10:45:32 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -408,8 +408,7 @@ def pulumi_program():
     from google.auth import impersonated_credentials
     import json
     from google.oauth2 import service_account
-    json_key = key.private_key.apply(lambda x: str(base64.b64decode(x)))
-    print(dir(json_key))
+    json_key = key.private_key.apply(lambda x: f"{base64.b64decode(x)}")
     print(json_key)
     # credentials, project_id = service_account.Credentials.from_service_account_info(json_key)
     # scope = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
