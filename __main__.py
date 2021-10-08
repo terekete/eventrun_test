c82e978bb8f81eaf27aab7e45b9a1acb831382b8
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-08 15:51:12 -0400

    fix: add

--- __main__.py
+++ __main__.py
@@ -406,7 +406,7 @@ def pulumi_program():
     import google.auth
     import json
     from google.oauth2 import service_account
-    print(obj.get(id=obj.id))
+    print(obj.get(id=obj.id, resource_name=obj.name))
     # credentials, project_id = service_account.Credentials.from_service_account_info(json_key)
     # scope = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
     # client = cloudbuild_v1.services.cloud_build.CloudBuildClient(credentials=credentials)
