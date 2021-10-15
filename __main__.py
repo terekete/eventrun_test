f79577bbd14571a7bf7189f78ce44a3f56214c3b
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-14 23:00:45 -0400

    fix: init

--- __main__.py
+++ __main__.py
@@ -491,6 +491,7 @@ for team in teams_diff:
 
 
 
+
 # service_account_info = json.load(open('tsbt.json'))
 # credentials = osa.Credentials.from_service_account_info(service_account_info)
 # cb_client = cloudbuild_v1.services.cloud_build.CloudBuildClient(credentials=credentials)
