038fc83572084d259c84977fd502b64c10c1525d
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-15 10:00:38 -0400

    fix: init

--- __main__.py
+++ __main__.py
@@ -467,7 +467,7 @@ for team in teams_diff:
         {
             "name": "gcr.io/cloud-builders/gcloud",
             "entrypoint": "bash",
-            "args": ["-c", "gcloud secrets versions access latest --secret=\"github\" --project=\"eventrun\" > /root/.ssh/id_rsa"]
+            "args": ["-c", "gcloud auth list"]
         }
     ]
     operation = cb_client.create_build(project_id='eventrun', build=build)
