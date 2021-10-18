91993956ad36c1ba45fefea9e45fbd83e1f924d0
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-18 13:08:54 -0400

    fix: add

--- scripts/run_sa.sh
+++ scripts/run_sa.sh
@@ -11,8 +11,8 @@ pulumi plugin install resource gcp v5.24.0
 cat DIFF_TEAM.txt | while read team
 do
     python /workspace/scripts/sa.py $team
-    # export GOOGLE_APPLICATION_CREDENTIALS="/workspace/${team}.json"
-    # python /workspace/scripts/iac.py $team
+    export GOOGLE_APPLICATION_CREDENTIALS="/workspace/${team}.json"
+    python /workspace/scripts/iac.py $team
 done
 # cat DIFF_TEAM.txt | while read team
 # do
