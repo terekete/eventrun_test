6992147053ee16946eceaca315baa75c9f9b4187
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-18 08:23:13 -0400

    fix: add

--- scripts/run_sa.sh
+++ scripts/run_sa.sh
@@ -6,14 +6,15 @@ set -e
 export PULUMI_CONFIG_PASSPHRASE=test
 pulumi login gs://eventrun-state
 pulumi plugin install resource gcp v5.23.0
-# python /workspace/scripts/sa.py
+
 
 cat DIFF_TEAM.txt | while read team
 do
     echo "team:" ${team}
     python /workspace/scripts/sa.py $team
+done
+cat DIFF_TEAM.txt | while read team
+do
     export GOOGLE_APPLICATION_CREDENTIALS="/workspace/${team}.json"
     python /workspace/scripts/iac.py $team
 done
-ls -la
-pwd
