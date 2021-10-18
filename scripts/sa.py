4387a3e831f6419acb3924d346fab66d0fd446e1
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-18 09:09:47 -0400

    fix: add

--- scripts/sa.py
+++ scripts/sa.py
@@ -87,3 +87,4 @@ if __name__ == "__main__":
     bq_client = gcs.Client()
     with open(team + '.json', 'wb') as file_obj:
         bq_client.download_blob_to_file('gs://team_auth/' + team + '/' + team + '.json', file_obj)
+
