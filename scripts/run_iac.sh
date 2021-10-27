bb95116083cc53ca7070cd6c9465821e24673955
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-27 10:29:51 -0400

    fix: builder

---
+++ scripts/run_iac.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+export PULUMI_CONFIG_PASSPHRASE=test
+pulumi login gs://eventrun-state
+
+
+cat DIFF_TEAM.txt | while read team
+do
+    python /workspace/scripts/iac.py $team
+done
+
