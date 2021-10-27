3f62ce952fe2d426aa5cc717516e2cc8d575fe2d
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-27 09:54:54 -0400

    fix: add

--- scripts/run_sa.sh
+++ scripts/run_sa.sh
@@ -12,9 +12,9 @@ cat DIFF_TEAM.txt | while read team
 do
     python /workspace/scripts/sa.py $team
 done
-cat DIFF_TEAM.txt | while read team
-do
-    export GOOGLE_APPLICATION_CREDENTIALS="/workspace/${team}.json"
-    python /workspace/scripts/iac.py $team
-done
+# cat DIFF_TEAM.txt | while read team
+# do
+#     export GOOGLE_APPLICATION_CREDENTIALS="/workspace/${team}.json"
+#     python /workspace/scripts/iac.py $team
+# done
 
