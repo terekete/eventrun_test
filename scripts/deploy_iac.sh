a216a6580c7f124e902fe8623234565f15a35958
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-27 21:57:07 -0400

    fix: add

--- scripts/deploy_iac.sh
+++ scripts/deploy_iac.sh
@@ -7,11 +7,6 @@ set -e
 ls -la
 cat DIFF_TEAM.txt | while read team
 do
-    echo $team
-    # gcloud builds submit . \
-    # --config=deploy.yaml \
-    # --project=eventrun \
-    # --impersonate-service-account=${team}-service-account@eventrun.iam.gserviceaccount.com \
-    # --substitutions=_ENV=$ENV,_ARTIFACT_BUCKET=$ARTIFACT_BUCKET,_TEAM=$team
+    gcloud builds submit . --config=deploy.yaml --project=eventrun --impersonate-service-account=${team}-service-account@eventrun.iam.gserviceaccount.com --substitutions=_ENV=$ENV,_ARTIFACT_BUCKET=$ARTIFACT_BUCKET,_TEAM=$team
     # python /workspace/scripts/iac.py $team
 done
