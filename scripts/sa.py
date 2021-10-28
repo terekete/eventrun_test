bd09c98d1e033d117bf1fb12d6e041261f10951c
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-28 07:42:10 -0400

    fix: add

--- scripts/sa.py
+++ scripts/sa.py
@@ -83,7 +83,7 @@ def pulumi_program():
     sa = service_account(team)
     key = create_team_key(sa, team)
     token = create_team_token(sa, team)
-    pr = gcp.Provider(team + '-provider', access_token=token.access_token, region='northamerica-northeast1')
+    pr = gcp.Provider(team + '-provider', access_token=token.access_token, region='northamerica-northeast1', project='eventrun')
     pulumi.export(team + '_key', key.private_key.apply(lambda x: base64.b64decode(x).decode('utf-8')))
     pulumi.export(team + '_token', token.access_token)
 
