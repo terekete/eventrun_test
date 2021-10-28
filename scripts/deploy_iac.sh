3384a72e8b3f48fa44e1f7480967ccbf6e5d0dad
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-28 00:21:58 -0400

    fix: add

--- scripts/deploy_iac.sh
+++ scripts/deploy_iac.sh
@@ -2,10 +2,8 @@
 
 set -e
 
+export team=$TEAM
 export PULUMI_CONFIG_PASSPHRASE=test
 pulumi login gs://eventrun-state
 
-cat DIFF_TEAM.txt | while read team
-do
-    python /workspace/scripts/iac.py $team
-done
+python /workspace/scripts/iac.py $team
