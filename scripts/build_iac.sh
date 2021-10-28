2ea518f546e7afc0b7966e0db0962e8783450779
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-28 01:45:31 -0400

    fix: add

--- scripts/build_iac.sh
+++ scripts/build_iac.sh
@@ -3,6 +3,5 @@ set -e
 
 cat DIFF_TEAM.txt | while read team
 do
-    gcloud auth activate-service-account ${team}-service-account@eventrun.iam.gserviceaccount.com
-    gcloud builds submit . --config=deploy.yaml --project=eventrun --verbosity=info --account=${team}-service-account@eventrun.iam.gserviceaccount.com --gcs-log-dir=gs://eventrun-logs --substitutions=_ENV=$ENV,_ARTIFACT_BUCKET=$ARTIFACT_BUCKET,_TEAM=$team,_STAGE_PROJECT=$STAGE_PROJECT
+    gcloud builds submit . --config=deploy.yaml --project=eventrun --verbosity=error --account=${team}-service-account@eventrun.iam.gserviceaccount.com --impersonate-service-account=${team}-service-account@eventrun.iam.gserviceaccount.com --gcs-log-dir=gs://eventrun-logs --substitutions=_ENV=$ENV,_ARTIFACT_BUCKET=$ARTIFACT_BUCKET,_TEAM=$team,_STAGE_PROJECT=$STAGE_PROJECT
 done
