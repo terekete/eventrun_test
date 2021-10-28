80e7db7a971f01ee3404d723aaed981993a17de1
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-10-27 23:22:09 -0400

    fix: add

--- scripts/deploy_iac.sh
+++ scripts/deploy_iac.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
+
 set -e
 
-# export PULUMI_CONFIG_PASSPHRASE=test
-# pulumi login gs://eventrun-state
+export PULUMI_CONFIG_PASSPHRASE=test
+pulumi login gs://eventrun-state
 
-ls -la
 cat DIFF_TEAM.txt | while read team
 do
-    gcloud builds submit . --config=deploy.yaml --project=eventrun --impersonate-service-account=${team}-service-account@eventrun.iam.gserviceaccount.com --gcs-log-dir=gs://eventrun-logs --substitutions=_ENV=$ENV,_ARTIFACT_BUCKET=$ARTIFACT_BUCKET,_TEAM=$team
-    # python /workspace/scripts/iac.py $team
+    python /workspace/scripts/iac.py $team
 done
-
