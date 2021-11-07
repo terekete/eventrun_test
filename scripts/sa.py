c6c850edeb00ca87f10dd23713c451f8aa41575a
tereketeketitaka@gmail.com <tereketeketitaka@gmail.com>
2021-11-07 10:26:27 -0500

    fix: add

--- scripts/sa.py
+++ scripts/sa.py
@@ -8,7 +8,7 @@ import google.auth
 
 from google.cloud import storage as gcs
 from pulumi.automation import errors
-from pulumi_gcp import storage, serviceaccount, projects, bigquery
+from pulumi_gcp import storage, serviceaccount, projects, bigquery, dataproc
 from pulumi import automation as auto
 
 
@@ -165,6 +165,46 @@ def pulumi_program():
             'write_disposition': 'WRITE_TRUNCATE',
             'query': 'select * from `intrepid-memory-321513.test_dataset.test_table_ext` limit 10'
         },
+        opts=pulumi.ResourceOptions(provider=pr)
+    )
+    proc = dataproc.Cluster(team + '_dataproc_cluster',
+        region='northamerica-northeast1',
+        graceful_decommission_timeout='120s',
+        labels ={
+            'foo': 'bar'
+        },
+        cluster_config=dataproc.ClusterClusterConfigArgs(
+            staging_bucket=bucket.id,
+            master_config=dataproc.ClusterClusterConfigMasterConfigArgs(
+                num_instances=1,
+                machine_type='e2-standard-4',
+                disk_config=dataproc.ClusterClusterConfigMasterConfigDiskConfigArgs(
+                    boot_disk_size_gb='pd-ssd',
+                    boot_disk_size_gb=10,
+                ),
+            ),
+            worker_config=dataproc.ClusterClusterConfigWorkerConfigArgs(
+                num_instances=1,
+                machine_type='e2-standard-2',
+                disk_config=dataproc.ClusterClusterConfigWorkerConfigDiskConfigArgs(
+                    boot_dist_size_gb=10,
+                    num_local_ssds=1,
+                ),
+            ),
+            preemptible_worker_config=dataproc.ClusterClusterConfigPreemptibleWorkerConfigArgs(
+                num_instances=0,
+            ),
+            software_config=dataproc.ClusterClusterConfigSoftwareConfigArgs(
+                image_version='1.3.7-deb9',
+                override_properties={
+                    'dataproc:dataproc.allow.zero.workers': 'true',
+                },
+            ),
+            gce_cluster_config=dataproc.ClusterClusterConfigGceClusterConfigArgs(
+                service_account=sa.email,
+                service_account_scopes=['cloud-platform']
+            ),
+        ),
         opts=pulumi.ResourceOptions(provider=pr)
     )
     # pulumi.export(team + '_key', key.private_key.apply(lambda x: base64.b64decode(x).decode('utf-8')))
